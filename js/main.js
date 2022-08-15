e1205672a013a6607085d914fb8a85653757add8
Sergey Goncharovр <doimpos@gmail.com>
2022-08-15 22:29:12 +0500

    Блок товаров

--- js/main.js
+++ js/main.js
@@ -5,4 +5,21 @@ $(function() {
         arrows: false,
         dots: true
     });
+    $('.prod_sl_js').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    fade: true,
+                    adaptiveHeight: true,
+                    dots: true
+                }
+            }
+        ]
+    });
 });
